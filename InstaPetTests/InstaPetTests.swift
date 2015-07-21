3cb256f23d97940da35bf2266cd643b8d8b993ad
Terry Wang <terry.wang@accenture.com>
2015-07-20 20:58:36 -0700

    Initial Commit

---
+++ InstaPetTests/InstaPetTests.swift
@@ -0,0 +1,36 @@
+//
+//  InstaPetTests.swift
+//  InstaPetTests
+//
+//  Created by Terry Wang on 7/20/15.
+//  Copyright (c) 2015 Vento LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class InstaPetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
